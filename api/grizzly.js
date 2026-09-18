4a781b3ef970e8ecd58d53f1c14e94e1449352e1
ziadsrour1001-ux <ziadsrour1001@gmail.com>
2026-09-18 17:41:04 +0300

    Update grizzly.js

--- api/grizzly.js
+++ api/grizzly.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // تفعيل الـ CORS الكامل للاتصال المباشر من المتجر
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -8,11 +7,9 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  // مفتاح الـ API الخاص بك في Grizzly
-  const API_KEY = process.env.GRIZZLY_API_KEY || "ضع_مفتاح_GRIZZLY_الخاص_بك_هنا";
+  const API_KEY = process.env.GRIZZLY_API_KEY || "205837984918fa408d1ee6ce337bf04e";
   const { action = "getPrices", ...params } = req.query;
 
-  // إعداد مسار الاتصال المباشر بخوادم Grizzly SMS
   const queryParams = new URLSearchParams({
     api_key: API_KEY,
     action: action,
@@ -30,7 +27,6 @@ export default async function handler(req, res) {
 
     const responseText = await upstreamRes.text();
 
-    // إرجاع النتيجة كـ JSON إذا كانت قائمة أسعار أو خدمات، أو كنص عادي لحالات الحجز والأكواد
     try {
       const parsedJson = JSON.parse(responseText);
       return res.status(200).json(parsedJson);
